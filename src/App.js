d924da8623c613f230d0ae086d464afd42719087
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2021-02-26 12:30:07 +0530

    update website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
+import Container from '@material-ui/core/Container';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <h3> Happy Birthday Farzeen ! </h3> 
+      <img src="https://scontent.fgau1-2.fna.fbcdn.net/v/t1.0-9/94274949_2513236805657535_3565206838641164288_o.jpg?_nc_cat=106&ccb=3&_nc_sid=09cbfe&_nc_ohc=vOoL5ASXJVMAX9RelRW&_nc_ht=scontent.fgau1-2.fna&oh=90437551fc4a42378abf3fc3ba5da6e8&oe=605EA37F" height="400" width="400" />
+      <p> This is not the best website I could build but I have the best of my feelings and wishes      for your birthday. You're 22 year old now and God has definitely turned you into one hell of a beautiful, gorgeous, intelligent and confident woman. You are now ready to take on this harsh world and I'm lucky to be by your side witnessing all of it. I feel very lucky to have you in my life every single day. 
+      </p>
+    <p> 
+    I understand you're going through a tough time in your life, but I truly believe that you'll conquer success by facing each every challenge bravely as you already have. I wish this year brings new people, new opportunities to grow in your life and takes you closer to living the dream.
+    </p>
+    </Container>
   );
 }
 
